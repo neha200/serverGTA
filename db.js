5a361d98a253d0cfe37e4fff5aa113b3b609b205
neha200 <52529855+neha200@users.noreply.github.com>
2022-12-09 14:43:12 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool= require('pg').Pool
 
 const pool = new Pool({
     user: "admin",
-    host: "postgres://admin:EeI3IwBJb6YywcCvPWmSV2z9PtRWba83@dpg-ce9doq82i3ms2102ae6g-a/gta_gs2d",
+    host: "postgres://admin:EeI3IwBJb6YywcCvPWmSV2z9PtRWba83@dpg-ce9doq82i3ms2102ae6g-a.oregon-postgres.render.com/gta_gs2d",
     database:"gta_gs2d",
     password:"EeI3IwBJb6YywcCvPWmSV2z9PtRWba83",
     port:5432

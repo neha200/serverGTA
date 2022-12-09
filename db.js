ecb9c861ab9718b3f257034c229527e554c78009
neha200 <52529855+neha200@users.noreply.github.com>
2022-12-09 15:42:04 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const Pool= require('pg').Pool
+const connectionString = 'postgres://admin:EeI3IwBJb6YywcCvPWmSV2z9PtRWba83@dpg-ce9doq82i3ms2102ae6g-a/gta_gs2d'
 
 const pool = new Pool({
-    postgres://admin:EeI3IwBJb6YywcCvPWmSV2z9PtRWba83@dpg-ce9doq82i3ms2102ae6g-a/gta_gs2d
+    connectionString
 });
 
 module.exports = pool;

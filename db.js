5121aa9aba7cf4bc9dd8cf1249a57f2dc158f357
neha200 <52529855+neha200@users.noreply.github.com>
2023-01-16 10:47:46 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Pool= require('pg').Pool
-const connectionString = 'postgres://admin:EeI3IwBJb6YywcCvPWmSV2z9PtRWba83@dpg-ce9doq82i3ms2102ae6g-a/gta_gs2d'
+const connectionString = "postgres://admin:EeI3IwBJb6YywcCvPWmSV2z9PtRWba83@dpg-ce9doq82i3ms2102ae6g-a.oregon-postgres.render.com/gta_gs2d"
 
 const pool = new Pool({
     connectionString:connectionString,
